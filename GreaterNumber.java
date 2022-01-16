189114ff115480517d2356ce7d578f8f7797b677
Surimani Niteesh <97540428+SurimaniNiteesh@users.noreply.github.com>
2022-01-16 20:23:05 +0530

    Add files via upload

---
+++ GreaterNumber.java
@@ -0,0 +1,24 @@
+package FirstHand_Jan_14_2022;
+
+public class GreaterNumber {
+
+	public static void main(String[] args) {
+		int num1=500;
+		int num2=2000;
+		int num3=1000;
+		if(num1>num2 && num1>num3) 
+		        {
+			System.out.print(num1+"is Greater");
+		        }
+		else if(num2>num1 && num2>num3) 
+		        {
+			System.out.print(num2+"is Greater");
+		        }
+		else
+		        {
+			System.out.print(num3+"is Greater");
+		        }
+
+	}
+
+}
